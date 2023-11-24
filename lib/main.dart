83b866b4060fc477b4f6d6568739fd0d792bb5dd
Vivi Ferliana Putri <tustika.nityasa@gmail.com>
2023-11-24 09:15:24 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:grocease/app.dart';
+import 'package:grocease/utils/constants/colors.dart';
+import 'package:grocease/utils/theme/theme.dart';
+
+void main() {
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
+  runApp(const App());
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:grocease/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/chip_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/text_field_theme.dart';
+import 'package:grocease/utils/theme/custom_themes/text_theme.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    chipTheme: TChipTheme.lightChipTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme,
+    checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.lightOutlinedButtonTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    chipTheme: TChipTheme.darkChipTheme,
+    appBarTheme: TAppBarTheme.darkAppBarTheme,
+    checkboxTheme: TCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.darkOutlinedButtonTheme,
+  );
+}
